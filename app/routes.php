cccc413acb571579cf6f1c9c5301965e84d40ce6
Karim <karim.elallame@gmail.com>
2015-03-30 17:06:54 +0200

    preparing fake data
    TODO : Match class

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,48 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+Route::get('/home', function(){
+	return View::make('home');
+});
+
+Route::get('/logout', function()
+{
+	Auth::logout();
+	return Response::make('You are now logged out. :(');
+});
+
+Route::get('/admin', array(
+	'before' => 'auth',
+	function()
+	{
+		return Response::make('admin panel');
+	}
+));
+
+Route::get('/signin', function()
+{
+    return View::make('signin');
+});
+Route::post('/signin', function()
+{
+	$credentials = Input::only('username', 'password');
+	if (Auth::attempt($credentials)) {
+		return Redirect::intended('/');
+	}
+	return Redirect::to('signin');
+});
+
+Route::get('/signup', function()
+{
+	return View::make('create_user_form');
+});
+Route::post('/signup', function()
+{
+	$user = new User;
+	$user->username 	= Input::get('username');
+	$user->password 	= Hash::make(Input::get('password'));
+	$user->email    	= Input::get('email');
+	$user->save();
+
+	return Response::make('User created!');
+});
